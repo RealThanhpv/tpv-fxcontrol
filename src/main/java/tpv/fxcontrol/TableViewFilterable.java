a1e6b82dbbded3eebd442292420111316658c427
RealThanhpv <fireinwinterfell@gmail.com>
2021-11-27 14:03:49 +0700

    add remove multiple items to TableViewFilterable

--- src/main/java/tpv/fxcontrol/TableViewFilterable.java
+++ src/main/java/tpv/fxcontrol/TableViewFilterable.java
@@ -61,7 +61,13 @@ public class TableViewFilterable<T> extends TableView<T> implements Filterable {
 
     }
 
-    public boolean removeItem(T item) {
+    public boolean removeItem(List<T> items) {
+        boolean r = mediator.getSource().removeAll(items);
+        reFilter();
+        return r;
+    }
+
+    public boolean removeItems(T item) {
         boolean r = mediator.getSource().remove(item);
         reFilter();
         return r;
