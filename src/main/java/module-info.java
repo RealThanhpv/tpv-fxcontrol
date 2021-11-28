178152e43ff306b322d6f1b81fc86ba1c902f60a
RealThanhpv <fireinwinterfell@gmail.com>
2021-11-28 15:48:40 +0700

    change build file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module tpv.fxcontrol {
     exports tpv.fxcontrol;
     exports tpv.fxcontrol.cell;
 
-    opens tpv.fxcontrol to javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.controls;
 
 }
