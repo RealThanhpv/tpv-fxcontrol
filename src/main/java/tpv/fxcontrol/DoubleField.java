b5abe6451f2ae81b54fc02e106ebc15023bae5c8
RealThanhpv <fireinwinterfell@gmail.com>
2021-12-20 08:46:44 +0700

    add DoubleField

---
+++ src/main/java/tpv/fxcontrol/DoubleField.java
@@ -0,0 +1,45 @@
+
+package tpv.fxcontrol;
+
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
+import javafx.util.StringConverter;
+
+public final class DoubleField extends TextField {
+    private static final Pattern validEditingState = Pattern.compile("-?(([1-9][0-9]*)|0)?(\\.[0-9]*)?");
+    private static final UnaryOperator<Change> filter = (c) -> {
+        String text = c.getControlNewText();
+        return validEditingState.matcher(text).matches() ? c : null;
+    };
+    private static final StringConverter<Double> converter = new StringConverter<Double>() {
+        public Double fromString(String s) {
+            return !s.isEmpty() && !"-".equals(s) && !".".equals(s) && !"-.".equals(s) ? Double.valueOf(s) : 0.0D;
+        }
+
+        public String toString(Double d) {
+            return d.toString();
+        }
+    };
+    private final TextFormatter<Double> textFormatter;
+
+    public DoubleField() {
+        this.textFormatter = new TextFormatter(converter, 0.0D, filter);
+        this.setTextFormatter(this.textFormatter);
+    }
+
+    public double getValue() {
+        return this.textFormatter.getValue();
+    }
+
+    public void setValue(double value) {
+        this.textFormatter.setValue(value);
+    }
+
+    public ObjectProperty<Double> valueProperty() {
+        return this.textFormatter.valueProperty();
+    }
+}
