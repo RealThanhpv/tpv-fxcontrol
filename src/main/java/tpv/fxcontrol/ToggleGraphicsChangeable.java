a5f75b8b25c543addf59d5429ab31d528fd0d236
RealThanhpv <fireinwinterfell@gmail.com>
2021-12-30 17:32:50 +0700

    update ToggleGraphicsChangeable

--- src/main/java/tpv/fxcontrol/ToggleGraphicsChangeable.java
+++ src/main/java/tpv/fxcontrol/ToggleGraphicsChangeable.java
@@ -20,6 +20,22 @@ public class ToggleGraphicsChangeable extends ToggleButton {
             }
         });
     }
+
+    public Node getG1() {
+        return g1;
+    }
+
+    public Node getG2() {
+        return g2;
+    }
+
+    public void setG1(Node g1) {
+        this.g1 = g1;
+    }
+
+    public void setG2(Node g2) {
+        this.g2 = g2;
+    }
 }
 
 
