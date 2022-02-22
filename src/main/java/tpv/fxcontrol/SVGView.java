029e30b3d044d21878c764302c614b0284c7252d
RealThanhpv <fireinwinterfell@gmail.com>
2022-02-22 16:22:19 +0700

    initialize null value for svgUrlProp

--- src/main/java/tpv/fxcontrol/SVGView.java
+++ src/main/java/tpv/fxcontrol/SVGView.java
@@ -2,14 +2,15 @@ package tpv.fxcontrol;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.css.Styleable;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import tpv.jfxsvg.SVGLoader;
 import javafx.scene.layout.StackPane;
 
 public class SVGView extends StackPane {
-    ImageView imv;
-    private final StringProperty svgUrlProp = new SimpleStringProperty(this, "url"){
+
+    private final StringProperty svgUrlProp = new SimpleStringProperty(this, "url", null){
         @Override
         protected void invalidated() {
             loadSVGNode(get());
