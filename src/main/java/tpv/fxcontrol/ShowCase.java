0d9822ab60e5ce822c3a2e4e21d912e60d343044
RealThanhpv <fireinwinterfell@gmail.com>
2022-06-10 15:57:54 +0700

    add custom Skin for TextArea

--- src/main/java/tpv/fxcontrol/ShowCase.java
+++ src/main/java/tpv/fxcontrol/ShowCase.java
@@ -2,17 +2,25 @@ package tpv.fxcontrol;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import tpv.fxcontrol.skin.TextAreaExtendableSkin;
+import tpv.fxcontrol.skin.TextInputControlSkin;
 
 public class ShowCase extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         AnchorPane root = new AnchorPane();
         Scene scene = new Scene(root, 600, 400);
-        root.getChildren().add(new NullableColorPicker());
+        TextArea textArea = new TextArea();
 
-        org.scenicview.ScenicView.show(scene);
+        TextAreaExtendableSkin skin = new TextAreaExtendableSkin(textArea);
+        textArea.setSkin(skin);
+
+        root.getChildren().add(textArea);
+
+//        org.scenicview.ScenicView.show(scene);
         stage.setScene(scene);
         stage.show();
 
